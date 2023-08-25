627dd2cfe2ff1e6ab9c9ffe5b23079a98d63356d
Vardhman <113521400+Vardhman2712@users.noreply.github.com>
2023-08-25 14:17:34 +0530

    Upload Qualify the round.java
    Let's break down the code into simple steps:
    
    1. **Import Statements:** The code begins with import statements. These statements tell the program to include certain classes from Java's standard library that will be used later in the code.
    
    2. **Class Definition:** The code defines a class named `Codechef`. In Java, all the code needs to be within a class, and the name of the class should match the filename.
    
    3. **Main Method:** The program defines a method named `main`, which is the entry point of the program. This method is executed when the program starts running.
    
    4. **Taking Input:** Inside the `main` method, the program creates a `Scanner` object (`sc`) to read input from the standard input (keyboard). It then reads an integer `T`, which represents the number of test cases.
    
    5. **Test Case Loop:** The program enters a loop that iterates `T` times. In each iteration, it performs the following steps for a single test case:
       - Reads two integers, `A` and `B`, representing the scores in two categories.
       - Reads an integer `X`, which is the minimum combined score needed for qualification.
       
    6. **Eligibility Check:** The program checks whether the combined score `(A * 1) + (B * 2)` is greater than or equal to `X`. If this condition is true, it means the participant qualifies. If the condition is false, it means the participant does not qualify.
    
    7. **Print Result:** Depending on the eligibility check result, the program prints either "Qualify" or "NotQualify" to the standard output (console).
    
    8. **Loop End:** After the loop completes, the program finishes running.
    
    In summary, this code reads the number of test cases, and for each test case, it reads two category scores and a minimum combined score. Then, it calculates the combined score for the participant, checks if it meets the qualification threshold, and prints the result accordingly. This code can be used to determine whether participants qualify for a competition based on their scores in two categories.

---
+++ Qualify the round.java
@@ -0,0 +1,26 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		while(T-->0) {
+		    int X = sc.nextInt();
+		    int A = sc.nextInt();
+		    int B = sc.nextInt();
+		    if ((A*1)+(B*2)>=X) {
+		        System.out.println("Qualify");
+		    } else {
+		        System.out.println("NotQualify");
+		    }
+		}
+	}
+}
