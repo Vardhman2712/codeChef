1109c09201589fddbd0eac8a4972cff48ac55321
Vardhman <113521400+Vardhman2712@users.noreply.github.com>
2023-10-05 15:28:49 +0530

    Create Get Subscription.java
    # Explanation:
    
    1. Scanner is used to take input from the user.
    2.  T is the number of test cases.
    3. The while loop iterates through each test case.
    4. For each test case, it reads an integer X.
    5. It checks whether X is less than or equal to 30.
    6. Prints "No" if the condition is true; otherwise, prints "Yes".
    
    - If you have any specific questions or if there's something else you'd like to know or modify, feel free to ask!

---
+++ Get Subscription.java
@@ -0,0 +1,16 @@
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		while(T-->0) {
+		    int X = sc.nextInt();
+		    if (X <= 30) {
+		        System.out.println("No");
+		    } else {
+		        System.out.println("Yes");
+		    }
+		}
+	}
+}
