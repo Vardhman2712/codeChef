f847a1b07a6eba619c2270164c174efae186083e
Vardhman <113521400+Vardhman2712@users.noreply.github.com>
2023-10-06 23:16:07 +0530

    Create Lunchtime.java
    Here are the steps of the provided Java code:
    
    1. **Input Reading:**
       - Read the number of test cases `T` using `sc.nextInt()`.
    
    2. **Test Case Loop:**
       - Use a `while` loop to iterate through each test case, decrementing `T` in each iteration (`while (T-- > 0)`).
    
    3. **Read Input Value:**
       - Read the input value `X` for the current test case using `sc.nextInt()`.
    
    4. **Conditional Check:**
       - Check if the value of `X` is between 1 and 4 (inclusive) using the condition `if (X >= 1 && X <= 4)`.
    
    5. **Output:**
       - If the condition is true, print "Yes" using `System.out.println("Yes")`.
       - If the condition is false, print "No" using `System.out.println("No")`.
    
    6. **Repeat:**
       - Repeat the loop for the remaining test cases.
    
    In summary, the code reads the number of test cases and for each test case, it reads an integer `X`. It then checks if `X` is between 1 and 4 (inclusive) and prints "Yes" or "No" accordingly. This process is repeated for all the test cases.

---
+++ Lunchtime.java
@@ -0,0 +1,16 @@
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		while (T-->0) {
+		    int X = sc.nextInt();
+		    if (X >= 1 && X <= 4) {
+		        System.out.println("Yes");
+		    } else {
+		        System.out.println("No");
+		    }
+		}
+	}
+}
