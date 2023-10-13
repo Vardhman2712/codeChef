a04f4a287d4447ecc8e32ed5ec129cff2e738ccc
Vardhman <113521400+Vardhman2712@users.noreply.github.com>
2023-10-13 23:19:08 +0530

    Create Sugarcane Juice Business.java
    Here's an explanation of the given Java code in bullet points:
    
    - **Objective:**
      - The code is designed to read an integer `T` representing the number of test cases. For each test case, it reads an integer `N` and outputs the result of a calculation.
    
    - **Input Reading:**
      - It uses the `Scanner` class to read input from the standard input stream (`System.in`).
      - Reads the number of test cases `T` from the input.
    
    - **Test Case Loop:**
      - Utilizes a `while` loop to iterate through each test case.
    
    - **Test Case Processing:**
      - Inside the loop, it reads an integer `N` from the input.
      - Outputs the result of the calculation `15 * N` on a new line using `System.out.println()`.
    
    - **Calculation:**
      - The calculation `15 * N` represents the number of coins a person makes by selling one glass. It seems to imply that for each glass sold, the person earns 15 coins.
    
    - **Output:**
      - Prints the result of the calculation for each test case on a new line.
    
    - **Time Complexity:**
      - The time complexity is O(T), where T is the number of test cases, as the loop runs T times.
    
    - **Space Complexity:**
      - The space complexity is O(1) since there is no significant memory usage that scales with the input size.
    
    - **Exception Handling:**
      - The `main` method declares that it may throw a `java.lang.Exception`, but in this specific code, there's no explicit throwing of exceptions during runtime. The `throws java.lang.Exception` in the method signature is not necessary for this specific code and can be removed.

---
+++ Sugarcane Juice Business.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		while (T-->0) {
+		    int N = sc.nextInt();
+		    System.out.println(15*N); // He makes 15 coins by selling one glass
+		}
+	}
+}
