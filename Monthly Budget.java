069420eb67946db49ae7a1b14f3a1dfcf558f728
Vardhman <113521400+Vardhman2712@users.noreply.github.com>
2023-10-17 23:54:23 +0530

    Create Monthly Budget.java
    Here's an explanation of the given Java code in bullet points:
    
    - **Objective:**
      - The code is designed to read an integer `T` representing the number of test cases. For each test case, it reads two integers `X` and `Y` and determines whether `X/30` is greater than or equal to `Y`.
    
    - **Input Reading:**
      - It uses the `Scanner` class to read input from the standard input stream (`System.in`).
      - Reads the number of test cases `T` from the input.
    
    - **Test Case Loop:**
      - Utilizes a `while` loop to iterate through each test case.
    
    - **Input Reading (cont.):**
      - Inside the loop, it reads two integers `X` and `Y` from the input.
    
    - **Comparison and Output:**
      - Checks whether `X/30` is greater than or equal to `Y`.
        - If true, it prints "YES."
        - If false, it prints "NO."
    
    - **Time Complexity:**
      - The time complexity is O(T), where `T` is the number of test cases, as the loop runs T times.
    
    - **Space Complexity:**
      - The space complexity is O(1) since there is no significant memory usage that scales with the input size.
    
    - **Logic:**
      - The code is checking if the integer division of `X` by `30` is greater than or equal to `Y`. This is checking if there are at least `Y` multiples of 30 in `X`. If true, it indicates that `X` is sufficiently large to cover `Y` days based on a daily rate of 30.

---
+++ Monthly Budget.java
@@ -0,0 +1,17 @@
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		while(T-->0) {
+		    int X = sc.nextInt();
+		    int Y = sc.nextInt();
+		    if (X/30 >= Y) {
+		        System.out.println("YES");
+		    } else {
+		        System.out.println("NO");
+		    }
+		}
+	}
+}
