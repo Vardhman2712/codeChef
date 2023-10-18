2b147a71856e797023e9eb235555bcc6de3f1978
Vardhman <113521400+Vardhman2712@users.noreply.github.com>
2023-10-18 20:58:49 +0530

    Create Credit score.java
    Let's break down the provided Java code into key points:
    
    1. **Class Declaration:**
       - `class Codechef {`: Defines a class named `Codechef`.
    
    2. **Main Method:**
       - `public static void main(String[] args) throws java.lang.Exception {`: Entry point of the program.
    
    3. **Scanner Initialization:**
       - `Scanner sc = new Scanner(System.in);`: Creates a Scanner object named `sc` to read input from the standard input stream.
    
    4. **Input Reading:**
       - `int x = sc.nextInt();`: Reads an integer from the user input and stores it in the variable `x`.
    
    5. **Conditional Statement:**
       - `if (x >= 750) {`: Checks if the value of `x` is greater than or equal to 750.
    
    6. **Output Printing:**
       - `System.out.println("YES");`: If the condition is true, prints "YES" to the standard output.
       - `System.out.println("NO");`: If the condition is false, prints "NO" to the standard output.
    
    7. **Scanner Closure:**
       - `sc.close();`: Closes the Scanner to release system resources. It's important to close the Scanner to avoid resource leaks.
    
    8. **Exception Handling:**
       - `throws java.lang.Exception`: The `main` method declares that it can throw a generic `Exception`. However, in this specific code, it's not used for handling any exceptions.
    
    9. **Note:**
       - The code assumes that the user provides a valid integer input. If the user enters a non-integer value, it will result in an `InputMismatchException`.
    
    10. **Closing Remark:**
       - This simple program reads an integer, checks if it's greater than or equal to 750, and prints "YES" or "NO" accordingly.

---
+++ Credit score.java
@@ -0,0 +1,15 @@
+class Codechef {
+    public static void main(String[] args) throws java.lang.Exception {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+
+        if (x >= 750) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+
+        sc.close(); // Close the scanner to avoid resource leak
+    }
+}
