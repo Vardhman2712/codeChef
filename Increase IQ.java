25e19aee58df82609cde1ae19a89cb3970605dc7
Vardhman <113521400+Vardhman2712@users.noreply.github.com>
2023-10-20 23:23:36 +0530

    Create Increase IQ.java
    Let's break down the provided Java code into key points:
    
    1. **Class Declaration:**
       - `class Codechef {`: Defines a class named `Codechef`.
    
    2. **Main Method:**
       - `public static void main(String[] args) throws java.lang.Exception {`: Entry point of the program.
    
    3. **Scanner Initialization:**
       - `Scanner sc = new Scanner(System.in);`: Creates a `Scanner` object to read input from the standard input stream.
    
    4. **Input Reading:**
       - `int X = sc.nextInt();`: Reads an integer `X` from the user input.
    
    5. **Conditional Check:**
       - `if ((X + 7) > 170) {`: Checks if the sum of `X` and 7 is greater than 170.
    
    6. **Output Printing:**
       - If the condition is true, it prints "YES" to the standard output using `System.out.println("YES");`.
       - If the condition is false, it prints "NO" to the standard output using `System.out.println("NO");`.
    
    7. **Exception Handling:**
       - `throws java.lang.Exception`: The `main` method declares that it can throw a generic `Exception`. However, in this specific code, it's not used for handling any exceptions.
    
    8. **Overall Functionality:**
       - This code reads an integer `X` from the user, adds 7 to it, and checks if the result is greater than 170. Depending on the condition, it prints "YES" or "NO," indicating whether the sum of `X` and 7 is greater than 170.
    
    9. **Note:**
       - The code assumes that the user provides a valid integer input. If the user enters a non-integer value, it may result in an `InputMismatchException`.

---
+++ Increase IQ.java
@@ -0,0 +1,13 @@
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int X = sc.nextInt();
+		if ((X+7)>170) {
+		    System.out.println("YES");
+		} else {
+		    System.out.println("NO");
+		}
+	}
+}
