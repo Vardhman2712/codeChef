da4b530d496a6d0f794c71539bd726d8b2afec1c
Vardhman <113521400+Vardhman2712@users.noreply.github.com>
2023-10-21 23:58:01 +0530

    Create Battery Low.java
    Let's break down the provided Java code into key points:
    
    1. **Class Declaration:**
       - `class Codechef {`: Defines a class named `Codechef`.
    
    2. **Main Method:**
       - `public static void main(String[] args) throws java.lang.Exception {`: Entry point of the program.
    
    3. **Scanner Initialization:**
       - `Scanner sc = new Scanner(System.in);`: Creates a `Scanner` object to read input from the standard input stream.
    
    4. **Input Reading:**
       - `int T = sc.nextInt();`: Reads an integer `T` from the user input, representing the number of test cases.
    
    5. **Test Case Loop:**
       - `while (T-- > 0) {`: Initiates a loop that iterates for each test case, decrementing `T` in each iteration.
    
    6. **Input Reading within Loop:**
       - `int X = sc.nextInt();`: Reads an integer `X` for each test case.
    
    7. **Conditional Check:**
       - `if (X <= 15) {`: Checks if the value of `X` is less than or equal to 15.
    
    8. **Output Printing:**
       - If the condition is true, it prints "YES" to the standard output using `System.out.println("YES");`.
       - If the condition is false, it prints "NO" to the standard output using `System.out.println("NO");`.
    
    9. **Loop Continuation:**
       - The loop continues for the next test case until `T` becomes zero.
    
    10. **Exception Handling:**
        - `throws java.lang.Exception`: The `main` method declares that it can throw a generic `Exception`. However, in this specific code, it's not used for handling any exceptions.
    
    11. **Overall Functionality:**
        - This code reads an integer `T` representing the number of test cases and an integer `X` for each test case. For each test case, it checks if `X` is less than or equal to 15 and prints "YES" or "NO" accordingly, indicating whether the value of `X` meets the condition.
    
    12. **Note:**
        - The code assumes that the user provides a valid integer input. If the user enters a non-integer value, it may result in an `InputMismatchException`.

---
+++ Battery Low.java
@@ -0,0 +1,16 @@
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		while (T-->0) {
+		    int X = sc.nextInt();
+		    if (X<=15) {
+		        System.out.println("YES");
+		    } else {
+		        System.out.println("NO");
+		    }
+		}
+	}
+}
