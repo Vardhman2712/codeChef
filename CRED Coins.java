209cd1bae0c253cc0a7aa27c146b6faef3c1fa0c
Vardhman <113521400+Vardhman2712@users.noreply.github.com>
2023-12-21 22:29:33 +0530

    Create CRED Coins.java
    Let's break down the provided Java code into bullet points:
    
    - **Input:**
      - The code takes input from the standard input (System.in) using a Scanner.
    
    - **Number of Test Cases:**
      - Read an integer `T`, representing the number of test cases.
    
    - **Test Case Loop:**
      - Use a `while` loop to iterate through each test case (decrementing `T` after each iteration).
    
    - **Test Case Processing:**
      - Read two integers `X` and `Y` as input.
      - Calculate the product of `X` and `Y`.
      - Divide the product by 100.
      - Print the result to the standard output (System.out).
    
    - **Explanation:**
      - The code calculates the result for each test case by taking the product of two integers `X` and `Y` and then dividing the result by 100.
      - The code assumes that the input values are non-negative integers.
    
    - **Time Complexity:**
      - The time complexity is O(T), where T is the number of test cases.
    
    - **Space Complexity:**
      - The space complexity is O(1) as the code uses a constant amount of space regardless of the input size.

---
+++ CRED Coins.java
@@ -0,0 +1,13 @@
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		while (T-->0) {
+		    int X = sc.nextInt();
+		    int Y = sc.nextInt();
+		    System.out.println((X*Y)/100);
+		}
+	}
+}
